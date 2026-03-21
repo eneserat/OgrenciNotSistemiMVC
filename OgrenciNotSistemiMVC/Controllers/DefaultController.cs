585f189dab36f85e32e97c945354a561dd91ac32
eneserat <erateeness@gmail.com>
2026-03-21 23:34:10 +0300

    Add project files.

---
+++ OgrenciNotSistemiMVC/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OgrenciNotSistemiMVC.Models.EntityFramework;
+
+namespace OgrenciNotSistemiMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbMvcOkulEntities db = new DbMvcOkulEntities();
+     
+        public ActionResult Index()
+        {
+            var derslistele = db.TBL_DERSLER.ToList();
+            return View(derslistele);
+        }
+    }
+}

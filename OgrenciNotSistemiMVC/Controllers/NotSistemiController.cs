b6a8f09f8ac0bd266a13edd835ac41a68ce28abc
eneserat <erateeness@gmail.com>
2026-03-22 01:25:12 +0300

    Öğrenci Ekleme View & Controllerı Tamamlandı

---
+++ OgrenciNotSistemiMVC/Controllers/NotSistemiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OgrenciNotSistemiMVC.Models.EntityFramework;
+
+
+namespace OgrenciNotSistemiMVC.Controllers
+{
+    public class NotSistemiController : Controller
+    {
+        DbMvcOkulEntities db = new DbMvcOkulEntities();
+        public ActionResult Index()
+        {
+            var notlistesi = db.TBL_NOTLAR.ToList();
+            return View(notlistesi);
+        }
+    }
+}
--- OgrenciNotSistemiMVC/Controllers/OgrenciController.cs
+++ OgrenciNotSistemiMVC/Controllers/OgrenciController.cs
@@ -1,19 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OgrenciNotSistemiMVC.Models.EntityFramework;
+using PagedList;
 namespace OgrenciNotSistemiMVC.Controllers
 {
     public class OgrenciController : Controller
     {
       DbMvcOkulEntities db = new DbMvcOkulEntities();
-        public ActionResult Index()
+        public ActionResult Index(int sayfa = 1)
         {
-            var ogrencılıstele = db.TBL_OGRENCILER.ToList();
+            var ogrencılıstele = db.TBL_OGRENCILER.ToList().ToPagedList(sayfa, 10);
 
             return View(ogrencılıstele);
+        }
+        public ActionResult ogrencıekle()
+        {
+            ViewBag.Ogrencıler = new SelectList(db.TBL_OGRENCILER, "OGRENCIID", "OGRENCIAD");
+
+            return View();
+        }
+
+        [HttpPost]
+       
+        public ActionResult OgrenciEkle(TBL_OGRENCILER ogrenci, HttpPostedFileBase OgrFoto)
+        {
+            if (OgrFoto != null && OgrFoto.ContentLength > 0)
+            {
+                var uploadDir = Server.MapPath("~/Uploads/Ogrenciler/");
+                if (!Directory.Exists(uploadDir)) Directory.CreateDirectory(uploadDir);
+
+                var fileName = Path.GetFileName(OgrFoto.FileName);
+                var path = Path.Combine(uploadDir, fileName);
+                OgrFoto.SaveAs(path);
+                ogrenci.OGRENCIFOTOGRAF = "/Uploads/Ogrenciler/" + fileName;
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.TBL_OGRENCILER.Add(ogrenci);
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "Öğrenci Sisteme Başarıyla Kaydedildi";
+                return RedirectToAction("Index");
+            }
+
+           
+            ViewBag.Dersler = new SelectList(db.TBL_DERSLER, "DERSID", "DERSAD");
+            return View(ogrenci);
         }
     }
 }

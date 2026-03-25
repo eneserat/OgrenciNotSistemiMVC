1bb7fc80fde38fe43c66b971bb1663701f5cc3b0
eneserat <erateeness@gmail.com>
2026-03-25 20:36:36 +0300

    kulüp controller ve view tamamlandı

--- OgrenciNotSistemiMVC/Controllers/KulupController.cs
+++ OgrenciNotSistemiMVC/Controllers/KulupController.cs
@@ -15,5 +15,27 @@ namespace OgrenciNotSistemiMVC.Controllers
             var kulupler = db.TBL_KULUPLER.ToList();
             return View(kulupler);
         }
+        [HttpGet]
+        public ActionResult YeniKulup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniKulup(TBL_KULUPLER p2)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TBL_KULUPLER.Add(p2);
+                db.SaveChanges();
+
+                TempData["SuccessMessage"] = "Kulüp Sisteme Başarıyla Eklendi!";
+                return RedirectToAction("YeniKulup"); 
+            }
+
+            return View(p2); 
+
+
+
+        }
     }
 }
